7d969cec87a9393f54a51b0a0458b5b09a03cb63
PaulPrabahar <prabahar1080@gmail.com>
2023-11-27 14:50:13 +0530

    Basic of operators in php.

---
+++ operators.php
@@ -0,0 +1,35 @@
+<?php
+//Operators
+
+//Airthematic operators.
+$number1 = 5;
+$number2 = 2;
+$answer = null;
+
+// '+' - addition.
+// '-' - Subracton.
+// '*' - Multiplication.
+// '**' - Power.
+// '/' - division returen quotient.
+// '%' - division returen reminder.
+
+// $answer = $number1 + $number2;
+// $answer = $number1 - $number2;
+// $answer = $number1 * $number2;
+$answer = $number1 ** $number2;
+// $answer = $number1 / $number2;
+// $answer = $number1 % $number2;
+echo "answer{$answer}<br>";
+
+// Icreament/decreament operators
+
+$counter = 0;
+$counter1 = 1;
+// $counter++; //increament
+// $counter--; //decreament
+
+// $counter1 -= 3; // minus equal: "minus provided number with a existing number"
+// $counter1 += 3; // add equal: "add provided number with a existing number"
+
+echo "counter: {$counter}<br>";
+echo "counter1: {$counter1}<br>";
