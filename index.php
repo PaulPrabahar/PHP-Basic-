1ce6c7f24ce554deb7a9c4c7c8a3a4727e4d8f35
PaulPrabahar <prabahar1080@gmail.com>
2023-11-29 16:27:59 +0530

    include function in php.

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
-include 'challange5.php';
+
+/*include() is used to copy the content of one file (php/html/text)
+and includes it in the php file which make are code reusable and easy to maintain.*/
+
+include 'sanitize_validate.php';//After include just mention the name of the file.
