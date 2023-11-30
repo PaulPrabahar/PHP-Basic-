2af516763557251befcfbb84775566f73ff27ed3
PaulPrabahar <prabahar1080@gmail.com>
2023-11-30 12:37:43 +0530

    index page

---
+++ index1.php
@@ -0,0 +1,6 @@
+<?php
+
+/*include() is used to copy the content of one file (php/html/text)
+and includes it in the php file which make are code reusable and easy to maintain.*/
+
+include 'session.php'; //After include just mention the name of the file.
